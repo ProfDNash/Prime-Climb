732cc7e4d252cb6c1a314893c6740fd933b7996d
David A. Nash <47865752+ProfDNash@users.noreply.github.com>
2020-03-27 10:38:12 -0400

    Create BasicGameData.py

---
+++ BasicGameData.py
@@ -0,0 +1,31 @@
+"""
+PLAYER class, INITGAME function, ROLLGENERATOR function
+
+@author: David A. Nash
+"""
+
+import numpy as np
+
+
+class Player:
+    def __init__(self, position, cards, cursed):
+        self.position = position  ##An ordered int pair (a,b) with a<b unless a=b=0 or a=b=101
+        self.cards = cards   ##An int list of the cards in the player's hand
+        self.cursed = cursed  ##True means player can only use - or / on next turn
+        
+def initGame(numPlayers):
+    global PlayerList
+    global Deck
+    if numPlayers<1 or numPlayers>4:
+        print("Error, can only be played with 1 to 4 players")
+    else:
+        PlayerList = [Player([0,0],[], False), Player([0,0],[], False), Player([0,0],[], False), Player([0,0],[], False)]
+        PlayerList = PlayerList[:numPlayers]
+        ##Shuffle the deck of cards
+        Deck = np.random.permutation(Deck)
+    return PlayerList
+
+def rollGenerator(n):
+    r = np.random.randint(1,high=n+1,size=2)
+    return r
+
