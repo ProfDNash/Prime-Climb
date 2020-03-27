43615d5e304bf43330ffbc9a28c918b9b7d5a9e0
David A. Nash <47865752+ProfDNash@users.noreply.github.com>
2020-03-27 19:26:01 -0400

    Update BasicGameData.py

--- BasicGameData.py
+++ BasicGameData.py
@@ -13,8 +13,7 @@ class Player:
         self.cards = cards   ##An int list of the cards in the player's hand
         self.cursed = cursed  ##True means player can only use - or / on next turn
         
-def initGame(numPlayers):
-    global Deck
+def initGame(numPlayers, Deck):
     if numPlayers<1 or numPlayers>4:
         print("Error, can only be played with 1 to 4 players")
     else:
@@ -22,7 +21,7 @@ def initGame(numPlayers):
         PlayerList = PlayerList[:numPlayers]
         ##Shuffle the deck of cards
         Deck = np.random.permutation(Deck)
-    return PlayerList
+    return PlayerList, Deck
 
 def rollGenerator(n):
     r = np.random.randint(1,high=n+1,size=2)
