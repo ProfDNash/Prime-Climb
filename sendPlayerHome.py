e183c3b146d6027cc96f88847209301c34a965ec
David A. Nash <47865752+ProfDNash@users.noreply.github.com>
2020-03-27 15:15:00 -0400

    Create sendPlayerHome.py

---
+++ sendPlayerHome.py
@@ -0,0 +1,41 @@
+"""
+SEND PLAYER HOME FUNCTION
+input: card -- an integer representing the card (can only send home if card==10 or card==11)
+       playerNum -- the index of the current player
+       PlayerList -- a list of player classes
+       DiscardPile -- a list of the cards which have been discarded
+
+output: PlayerList -- updated with any new positions and hands
+        DiscardPile -- updated with added card played (if any)
+
+Note: (1) Due to the rules of Prime Climb, no pawns can leave position 101.
+
+@author: David A. Nash
+"""
+
+def sendPlayerHome(card, playerNum, PlayerList, DiscardPile):
+    if card !=10 and card != 11:
+        print('Error.  You cannot bump with card ', card)  ##for debugging
+    else:
+        ##check whether any other players are away from the start (position 0)
+        offStart = [] ##initialize a list of tuples of the form (player_index,pawn_index)
+        for j in range(len(PlayerList)):
+            if j== playerNum:
+                continue
+            else:
+                ##cannot bump pawns on 0 or 101
+                if PlayerList[j].position[1] !=0 and PlayerList[j].position[1]!=101:
+                    offStart.append([j,1])
+                    if PlayerList[j].position[0] !=0:  ##if pos[0]==101, then the game should be over
+                        offStart.append([j,0])
+        if len(offStart)>0:  ##then there are pawns to bump which aren't at the start
+            ##choose a random player and pawn from the list of options
+            playerChosen, pawnChosen = offStart(np.random.randint(0,len(offStart)))
+            ##bump that pawn back to start and discard the card played
+            PlayerList[playerChosen].position[pawnChosen]=0
+            PlayerList[playerChosen].position.sort() ##re-sort the position into increasing order
+            PlayerList[playerNum].cards.remove(card)
+            DiscardPile.append(card)
+        else:
+            print("No one to bump.")  ##for debugging
+    return PlayerList, DiscardPile
