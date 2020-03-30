f2dabf6dd4e05b67d391d49b585c8ff4f5b41388
David A. Nash <47865752+ProfDNash@users.noreply.github.com>
2020-03-30 12:23:37 -0400

    Create forwardStep.py
    Computes a single step of forward propagation

---
+++ forwardStep.py
@@ -0,0 +1,26 @@
+"""
+FORWARD STEP FUNCTION
+Perform one layer of forward propagation
+input: A_prev -- an np.array of activations from the previous layer, shape (size of previous layer, 1)
+       W -- weight matrix for the current layer, shape (size of current layer, size of previous layer)
+       b -- bias vector, shape (size of current layer, 1)
+       activiation -- a string calling either 'sigmoid' or 'relu' activation
+
+output: A -- activations for the current layer
+        cache -- a cache of (Z, W, and b) for the current layer to make backpropagation easier later
+
+@author: David A. Nash
+"""
+import numpy as np
+from NNutils import sigmoid, relu
+
+def forwardStep(A_prev, W, b, activation):
+    Z = np.dot(W,A_prev)+b
+    cache = (Z, W, b)
+    
+    if activation = 'sigmoid':
+        A = sigmoid(Z)
+    elif activation = 'relu':
+        A = relu(Z)
+    
+    return A, cache
