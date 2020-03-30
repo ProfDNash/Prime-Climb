6ed4ffa43f633d4b0c31306c3a0fe8ceb0990635
David A. Nash <47865752+ProfDNash@users.noreply.github.com>
2020-03-30 12:30:01 -0400

    added softmax option

--- forwardStep.py
+++ forwardStep.py
@@ -4,7 +4,7 @@ Perform one layer of forward propagation
 input: A_prev -- an np.array of activations from the previous layer, shape (size of previous layer, 1)
        W -- weight matrix for the current layer, shape (size of current layer, size of previous layer)
        b -- bias vector, shape (size of current layer, 1)
-       activiation -- a string calling either 'sigmoid' or 'relu' activation
+       activiation -- a string calling either 'sigmoid', 'relu', or 'softmax' activation
 
 output: A -- activations for the current layer
         cache -- a cache of (Z, W, and b) for the current layer to make backpropagation easier later
@@ -12,15 +12,17 @@ output: A -- activations for the current layer
 @author: David A. Nash
 """
 import numpy as np
-from NNutils import sigmoid, relu
+from NNutils import sigmoid, relu, softmax
 
 def forwardStep(A_prev, W, b, activation):
     Z = np.dot(W,A_prev)+b
     cache = (Z, W, b)
     
-    if activation = 'sigmoid':
+    if activation == 'sigmoid':
         A = sigmoid(Z)
-    elif activation = 'relu':
+    elif activation == 'relu':
         A = relu(Z)
+    elif activation == 'softmax':
+        A = softmax(A)
     
     return A, cache
