4f0a86e6851ef330093d0359d42518a3c5651541
David A. Nash <47865752+ProfDNash@users.noreply.github.com>
2020-03-30 12:47:40 -0400

    completed
    Performs forward propagation -- applying ReLU at each layer, until the final layer which is a softmax layer.

--- forwardProp.py
+++ forwardProp.py
@@ -3,8 +3,8 @@ FORWARD PROPAGATION (i.e. prediction)
 input: Xt -- input data from the t-th turn in the game
        parameters -- a dictionary containing parameters for each layer of the network
 
-output: nTurns -- the number of turns taken in the game
-        winner -- the winner of the game
+output: AL -- activations from the final layer of the network (should sum to 1)
+        caches -- a cache of the parameters and linear activations (Z) in each layer for backprop
 
 Note: (1) Due to the rules of Prime Climb, no pawns can leave position 101.
       (2) This function uses player classes and takeTurn (therefore, everything else)
@@ -12,14 +12,21 @@ Note: (1) Due to the rules of Prime Climb, no pawns can leave position 101.
 @author: David A. Nash
 """
 import numpy as np
-from NNutils import relu, softmax, sigmoid
+from forwardStep import forwardStep
 
 def forwardProp(Xt, parameters):  
     ##apply linear + ReLU at each layer until applying linear + softmax to the final layer##
-    L = len(parameters)/2  ##counts the number of layers
-    cache = {}  ##initialize cache to save values as we proceed
+    L = len(parameters)//2  ##counts the number of layers
+    caches = []  ##initialize caches list to save values as we proceed
+    A = Xt
     
-    for l in range(L-1):
-        cache['Z'+str(l+1)] = forward prop step
+    for l in range(1,L):
+        A_prev = A
+        A, cache = forwardStep(A_prev,parameters['W'+str(l)],parameters['b'+str(l)],'relu')
+        caches.append(cache)
+    
+    ##For layer L, compute softmax
+    AL, cache = forwardStep(A, parameters['W'+str(L)], parameters['b'+str(L)],'softmax')
+    caches.append(cache)
 
-    return cache
+    return AL, caches
