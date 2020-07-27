c29fca9de9dc8a5f3090027ea468b8f19ce622af
David A. Nash <47865752+ProfDNash@users.noreply.github.com>
2020-07-27 08:01:39 -0400

    Create OPtrain.py
    Create a training program for the single player game.

---
+++ OPtrain.py
@@ -0,0 +1,35 @@
+"""
+ONE PLAYER TRAINING FUNCTION
+Run through multiple games using the Learn Game function, to train NN
+
+input: parameters -- a dictionary containing parameters for each layer of the network, 'W1', 'b1', etc.
+       lambd -- parameter controlling the network's *memory* (lambd==1 perfect memory)
+       alpha -- learning rate parameter
+       numGames -- the number of games to play through
+       eps -- parameter controlling the amount of random exploration allowed in training
+
+output: turns -- an list containing the number of turns for each game
+        params -- updated parameters based on learning from one game
+
+
+@author: David A. Nash
+"""
+import numpy as np
+import pickle
+from onePlayer import onePlayerGame
+
+def OPtrain(parameters, lambd=0, alpha=0.01, numGames=10, eps=0.99):
+    ##initialize counters
+    turns = list()
+    params=parameters.copy()
+    for g in range(numGames):
+        if g%10==0: print('Game:',g)
+        Xlist, Ylist, turn, params = onePlayerGame(params, lambd, alpha, eps)
+        turns.append(turn)
+        eps *= 0.99
+        if g%100==0:
+            f = open('params.pkl', 'wb')
+            pickle.dump(params,f)
+            f.close()
+        
+    return turns, params
