051e4bd08900942cef80432987a2118289f811ab
David A. Nash <47865752+ProfDNash@users.noreply.github.com>
2020-07-29 11:52:51 -0400

    Update OPtrain.py
    Add flag to allow for games to be played without training

--- OPtrain.py
+++ OPtrain.py
@@ -7,6 +7,7 @@ input: parameters -- a dictionary containing parameters for each layer of the ne
        alpha -- learning rate parameter
        numGames -- the number of games to play through
        eps -- parameter controlling the amount of random exploration allowed in training
+       learn -- a boolean parameter controlling whether the network learns as it plays
 
 output: turns -- an list containing the number of turns for each game
         params -- updated parameters based on learning from one game
@@ -18,18 +19,22 @@ import numpy as np
 import pickle
 from onePlayer import onePlayerGame
 
-def OPtrain(parameters, lambd=0, alpha=0.01, numGames=10, eps=0.99):
+def OPtrain(parameters, lambd=0, alpha=0.01, numGames=10, eps=0.99, learn=True):
     ##initialize counters
     turns = list()
     params=parameters.copy()
     for g in range(numGames):
         if g%10==0: print('Game:',g)
-        Xlist, Ylist, turn, params = onePlayerGame(params, lambd, alpha, eps)
+        Xlist, Ylist, turn, params = onePlayerGame(params, lambd, alpha, eps, learn)
         turns.append(turn)
-        eps *= 0.99
-        if g%100==0:
-            f = open('params.pkl', 'wb')
-            pickle.dump(params,f)
-            f.close()
+        if learn==True:
+            eps *= 0.999
+            if g%100==0:
+                f = open('params3.pkl', 'wb')
+                pickle.dump(params,f)
+                f.close()
+                g = open('turns3.pkl', 'wb')
+                pickle.dump(turns,g)
+                g.close()
         
     return turns, params
