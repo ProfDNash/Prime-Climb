79a7afb731781ddd37557dd3b9f8f1e140d3f57b
Marty Nguyen <sendspamtomarty@gmail.com>
2021-01-31 10:49:52 -0500

    MTN#2/bumpChecker sometimes bumps wrong pawn

--- bumpChecker.py
+++ bumpChecker.py
@@ -7,26 +7,34 @@ output: PlayerList -- adjusted so that any pawns that have been landed on by
                       player[playerNum] get sent back to start.
 
 Note: (1) Due to the rules of Prime Climb, no pawns can leave position 101.
-      (2) This function makes use of the function cleanPositions
 
 @author: David A. Nash
 """
 import numpy as np
-from BasicGameData import Player
-from cleanPositions import cleanPositions
-
 
 def bumpChecker(playerNum, PlayerList):
+
     for i in range(len(PlayerList)):
-        if i == playerNum: ##self-bumping is taken care of in the generation of new positions
-            continue
-        ##Players cannot be bumped back from position 101
-        ##Check whether the current player's first pawn landed on one of player i's pawns
-        if 0<PlayerList[playerNum].position[0] <101 and PlayerList[playerNum].position[0] in PlayerList[i].position:
-            loc = np.where(PlayerList[playerNum].position[0] in PlayerList[i].position)[0][0]
-            PlayerList[i].position[loc] = 0
-        ##Check whether the current player's second pawn landed on one of player i's pawns
-        if 0<PlayerList[playerNum].position[1] <101 and PlayerList[playerNum].position[1] in PlayerList[i].position:
-            loc = np.where(PlayerList[playerNum].position[1] in PlayerList[i].position)[0][0]
-            PlayerList[i].position[loc] = 0
+
+        if i != playerNum: ##self-bumping is taken care of in the generation of new positions
+
+            bumped=False
+
+            ##Players cannot be bumped back from position 101
+            ##Check whether the current player's first pawn landed on one of player i's pawns
+            if 0<PlayerList[playerNum].position[0] <101 and PlayerList[playerNum].position[0] in PlayerList[i].position:
+               loc = np.where(PlayerList[playerNum].position[0] == PlayerList[i].position)[0][0]
+               PlayerList[i].position[loc] = 0
+               bumped=True
+
+            ##Check whether the current player's second pawn landed on one of player i's pawns
+            if 0<PlayerList[playerNum].position[1] <101 and PlayerList[playerNum].position[1] in PlayerList[i].position:
+                loc = np.where(PlayerList[playerNum].position[1] == PlayerList[i].position)[0][0]
+                PlayerList[i].position[loc] = 0
+                bumped=True
+
+            if bumped:
+                ## Sort positions
+                PlayerList[i].position = np.sort( PlayerList[i].position )
+
     return PlayerList
